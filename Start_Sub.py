033bdd19fbcec778193d2789af54da35a317f151
CCTV9999 <93818896+CCTV9999@users.noreply.github.com>
2025-04-16 15:44:10 +0800

    Create Start_Sub.py

---
+++ Start_Sub.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.Popen(["python", "./Ark.py"])
+subprocess.Popen(["python", "./DS.py"])
+subprocess.Popen(["python", "./SF.py"])
+subprocess.Popen(["python", "./NV.py"])
+subprocess.Popen(["python", "./AL.py"])
